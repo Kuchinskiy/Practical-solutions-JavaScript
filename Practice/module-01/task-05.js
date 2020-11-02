19b47d5702eda3320d8c07cec4c07d828b8f6300
Sergey <domarch388640@gmail.com>
2020-11-02 11:13:13 +0200

    commit

--- Practice/module-01/task-05.js
+++ Practice/module-01/task-05.js
@@ -18,3 +18,63 @@
 * Ямайка - 120 кредитов
 */
 
+const china = 100;
+const chili = 250;
+const australia = 170;
+const india = 80;
+const jamaica = 120;
+
+const deliveryChina = 'China';
+const deliveryChili = 'Chili';
+const deliveryAustralia = 'Australia';
+const deliveryIndia = 'India';
+const deliveryJamaica = ' Jamaica';
+
+let message;
+let price = 0;
+
+const promptLabel = `To which country would you like to arrange delivery: 
+${deliveryChina}, ${deliveryChili}, ${deliveryAustralia}, ${deliveryIndia}, ${deliveryJamaica} etc.`;
+
+const CANCEL = 'Canceled by user!';
+const NOT_DELIVERY = 'Delivery is not available in your country !!!';
+
+let userArrangesDelivery = prompt(promptLabel);
+
+if (userArrangesDelivery === null) {
+    message = CANCEL;
+} else {
+    // code
+
+    switch (userArrangesDelivery) {
+        case deliveryChina:
+            price = china;
+            break;
+
+        case deliveryChili:
+            price = chili;
+            break;
+
+        case deliveryAustralia:
+            price = australia;
+            break;
+
+        case deliveryIndia:
+            price = india;
+            break;
+
+        case deliveryJamaica:
+            price = jamaica;
+            break;
+    
+        default:
+            message = NOT_DELIVERY;
+            break;
+    }
+    if (price > 0 ) {
+        message = `Delivery to ${userArrangesDelivery} will cost ${price} credits`;
+    }
+}
+
+alert(message);
+
