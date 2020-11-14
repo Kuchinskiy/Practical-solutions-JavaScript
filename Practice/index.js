ad73479d4cc7a2ec0d236e9886fa3f3bf36b2973
Sergey <domarch388640@gmail.com>
2020-11-14 16:50:54 +0200

    commit

--- Practice/index.js
+++ Practice/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-// Reduce
+// Методы массивов Reduce, isArray
+// Найти самое большое число в массиве с помощью метода REDUCE:
+
 const arrNumbers = [5, -4, 7, -89, 0, 39, 8, 1, 2];
 
 let bigNumber = arrNumbers.reduce((acc, item) => {
@@ -10,4 +12,21 @@ let bigNumber = arrNumbers.reduce((acc, item) => {
     return acc;
 });
 
-console.log(bigNumber);
+// console.log(bigNumber);
+
+
+// Вывести все "id" без остальной информации хранящейся в массиве объектов:
+
+const arrObj = [
+    {id: 678876, city: 'Bonn', country: 'Germany'},
+    {id: 234432, city: 'Bern', country: 'Switzerland'},
+    {id: 987654, city: 'Las Vegas', country: 'USA'}
+];
+
+let idArr = arrObj.reduce((acc, item) => {
+    acc.push(item.id);
+    return acc;
+}, []);
+
+// console.log(idArr);
+
