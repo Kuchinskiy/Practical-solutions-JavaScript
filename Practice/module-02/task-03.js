aba93cc1e3bfe6be4063ed543dc745ef69f9fbc2
Sergey <domarch388640@gmail.com>
2021-07-23 16:26:28 +0300

    commit

--- Practice/module-02/task-03.js
+++ Practice/module-02/task-03.js
@@ -9,13 +9,22 @@
 */
 
 const formatString = function (string) {
-	for (let i = 0; i < string.length; i += 1) {
-		if (string.length < 40) {
-			return string;
-		}
+	// for (let i = 0; i < string.length; i += 1) { // в цикле "for" и еще одной проверке "if(условия)" нет надобности,
+	// 	if (string.length < 40) { // так как такой подход создает перизбыток в ненужном коде;
+	// 		return string;
+	// 	}
+
+		if (string.length > 40) {
+			return (`${string.slice(0, 40) + '...'}`);
+		// }
 	}
+	return string; // условие if(a) не выполняется и исполнение кода переходит далее;
 };
-// formatString('Curabitur ligula sapien, tincidunt non.')
+
+console.log(formatString('Curabitur ligula sapien.'));
 console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
 // вернется оригинальная строка
 
+console.log(formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',));
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// вернется форматированная строка
