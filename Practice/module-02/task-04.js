a59bd65109a4d21310992b1fa0251580026a64ca
Sergey <domarch388640@gmail.com>
2021-08-09 18:06:18 +0300

    commit

--- Practice/module-02/task-04.js
+++ Practice/module-02/task-04.js
@@ -1,7 +1,37 @@
 'use stricte';
 
 /*
-
+Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.
+Функция проверяет ее на содержание слов spam и sale. 
+Если нашли запрещенное слово то функция возвращает true, 
+если запрещенных слов нет функция возвращает false.
+Слова в строке могут быть в произвольном регистре.
 */
 
-console.log('hello');
+const checkForSpam = function(message) {
+	for (let i = 0; i < message.length; i += 1) {
+		if (message.includes('sale')) {
+			return true;
+		}
+
+		if (message.includes('spam')) {
+			return true;
+		}
+	}
+	return false;
+};
+
+/*
+Вызовы функции для проверки работоспособности твоей реализации.
+*/
+// console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+
+/*
+2.Для перебора елементов и проверки используем цикл for с операторами ветвления if...else
+3.Перевести все слова в нижний регистр для корректной проверки.
+*/
