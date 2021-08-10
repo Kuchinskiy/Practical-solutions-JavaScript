9dcb2fdc39c5bb0286f79f8d233be18d3dad642b
Sergey <domarch388640@gmail.com>
2021-08-10 10:23:42 +0300

    commit

--- Practice/module-02/task-04.js
+++ Practice/module-02/task-04.js
@@ -9,29 +9,24 @@
 */
 
 const checkForSpam = function(message) {
-	for (let i = 0; i < message.length; i += 1) {
-		if (message.includes('sale')) {
+		if (
+		message.toLowerCase().includes('sale') ||
+		message.toLowerCase().includes('spam')) {
 			return true;
 		}
 
-		if (message.includes('spam')) {
-			return true;
-		}
-	}
+		// if (message.toLowerCase().includes('spam')) {
+		// 	return true;
+		// }
+
 	return false;
 };
 
 /*
 Вызовы функции для проверки работоспособности твоей реализации.
 */
-// console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('Latest technology news')); // false
 console.log(checkForSpam('JavaScript weekly newsletter')); // false
 
 console.log(checkForSpam('Get best sale offers now!')); // true
-// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
-
-
-/*
-2.Для перебора елементов и проверки используем цикл for с операторами ветвления if...else
-3.Перевести все слова в нижний регистр для корректной проверки.
-*/
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
