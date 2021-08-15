35eff125405e4049adc12656e06c105fef8628d9
Sergey <domarch388640@gmail.com>
2021-08-15 15:29:08 +0300

    commit

--- Practice/index.js
+++ Practice/index.js
@@ -36,10 +36,10 @@ const evenNumbers = function (arr) {
 const arrNumbers = [5, -4, 7, -89, 0, 39, 8, 1, 2];
 
 let bigNumber = arrNumbers.reduce((acc, item) => {
-  if (item > acc) {
-    acc = item;
-  }
-  return acc;
+   if (item > acc) {
+	acc = item;
+   }
+   return acc;
 });
 
 // console.log(bigNumber);
@@ -47,14 +47,14 @@ let bigNumber = arrNumbers.reduce((acc, item) => {
 // Вывести все "id" без остальной информации хранящейся в массиве объектов:
 
 const arrObj = [
-  { id: 678876, city: 'Bonn', country: 'Germany' },
-  { id: 234432, city: 'Bern', country: 'Switzerland' },
-  { id: 987654, city: 'Las Vegas', country: 'USA' },
+   { id: 678876, city: 'Bonn', country: 'Germany' },
+   { id: 234432, city: 'Bern', country: 'Switzerland' },
+   { id: 987654, city: 'Las Vegas', country: 'USA' },
 ];
 
 let idArr = arrObj.reduce((acc, item) => {
-  acc.push(item.id);
-  return acc;
+   acc.push(item.id);
+   return acc;
 }, []);
 
 // console.log(idArr); // [678876, 234432, 987654]
