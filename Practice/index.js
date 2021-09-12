49a8a08170be1eb4c5187e0f7fa7c74dc745703d
Sergey <domarch388640@gmail.com>
2021-09-12 16:18:56 +0300

    commit

--- Practice/index.js
+++ Practice/index.js
@@ -105,7 +105,7 @@ const add = function (...args) {
 	return `Общая сумма: ${total}`;
 };
 
-console.log(add(1, 2, 3));
-console.log(add(1, 2, 3, 4, 5));
-console.log(add(1, 2, 3, 4, 5, 6, 7, 8, 9));
+// console.log(add(1, 2, 3));
+// console.log(add(1, 2, 3, 4, 5));
+// console.log(add(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
