b1ac9ac8b236e7a723c7ea9b37ebf2c210672969
Sergey <domarch388640@gmail.com>
2021-09-12 21:50:00 +0300

    commit

---
+++ Practice/module-03/task-01.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/*
+Напиши скрипт, который, для объекта user, последовательно:
+
+-- добавляет поле(key) mood со значением 'happy';
+-- заменяет значение hobby на 'skydiving';
+-- заменяет значение premium на false;
+
+-- выводит содержимое объекта user в формате 'ключ:значение'
+-- используя Object.keys() и for...of
+*/
+
+const user = {
+	name: 'Mango',
+	age: 20,
+	hobby: 'html',
+	premium: true,
+};
+
+// add --key:value
+user['mood'] = 'happy';
+
+// chenge -- values
+user.hobby = 'skydiving';
+user.premium = false;
+
+// return an array with object keys
+const keys = Object.keys(user);
+
+for (const key of keys) {
+	console.log(`${key}: ${user[key]}`);
+}
