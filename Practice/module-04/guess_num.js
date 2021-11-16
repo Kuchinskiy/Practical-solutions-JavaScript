28de31e58568743bfbfbdd83aad5963513aa75a7
Sergey <domarch388640@gmail.com>
2021-11-16 13:39:09 +0200

    commit

---
+++ Practice/module-04/guess_num.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const numSeven = Number(prompt('Введите число 7:'));
+const birthMonth = Number(prompt('Введите месяц вашего рождения:'));
+const birthDay = Number(prompt('Введите число вашего дня рождения:'));
+let total;
+
+const calcNumber = () => {
+	total = numSeven * birthMonth;
+	total -= 1;
+	total *= 13;
+
+	total += birthDay;
+	total += 3;
+	total *= 11;
+	total -= birthMonth;
+	total -= birthDay;
+	total /= 10;
+	total += 11;
+	total /= 100;
+
+	return total;
+};
+
+// console.log(calcNumber());
+// alert(`Вы родились месяц / дата: ${calcNumber()}`);
