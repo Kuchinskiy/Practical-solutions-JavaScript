9a0bd325ab800ee2c7159b3e95175b54f5f64cc6
Sergey <domarch388640@gmail.com>
2021-11-27 17:08:44 +0200

    commit

--- Practice/module-04/interActive.js
+++ Practice/module-04/interActive.js
@@ -1,21 +1,23 @@
 'use strict';
 
 /*
-Анимация элементов с помощью setInterval
+Анимация элементов HTML с помощью setInterval
 */
+
+// Временная функция 
 heading.onclick = function() {
 	// запомнить время начала
 	let start = Date.now();
-
+	// переменная timer  которой присваеваем функцию setInterval необходима для остановки инструкции
 	let timer = setInterval(function() {
-		//сколько времени прошло с начала анимации
+		//сколько времени прошло с начала анимации от 0 до 2000ms
 	let timePassed = Date.now() - start;
-	//
-	heading.style.left = timePassed / 5 + 'px';
+	//расстояние на которое необходимо сдивинуть элемент (2000ms / 10px = 200px)
+	// left - изменяет значение от 0рх до 200рх
+	heading.style.left = timePassed / 10 + 'px';
 	//закончить анимацию через 2 секунды
 	if (timePassed > 2000) {
 		clearInterval(timer);
-		return;
 	};
-	}, 20);
+	}, 10);
 };
