8ff36dff01d8ccf8a7fff04353df3c3a3b094c9d
Sergey <domarch388640@gmail.com>
2021-12-17 18:03:58 +0200

    complete

--- Practice/module-05/task-03.js
+++ Practice/module-05/task-03.js
@@ -1,5 +1,49 @@
 'use strict'
 
 /*
+Напиши класс Storage, который будет создавать объекты для управления складом товаров.
+При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
 
+Добавь методы класса:
+-- getItems() - возвращает массив текущих товаров
+-- addItem(item) - получает новый товар и добавляет его к текущим
+-- removeItem(item) - получет товар и, если он есть, удаляет его из текущих
 */
+
+class Storage {
+	constructor (items) {
+		this.items = items;
+	}
+
+	getItems() {
+		return this.items;
+	}
+
+	addItem(item) {
+		this.items.push(item);
+	}
+
+	removeItem(item) {
+		// https://www.delftstack.com/ru/howto/javascript/javascript-remove-from-array-by-value/
+		const inx = this.items.indexOf(item);
+		if (inx !== -1) {
+			this.items.splice(inx, 1);
+		}
+	}
+}
+
+const storage = new Storage([
+	'Нанитоиды',
+	'Пролонгер',
+	'Железные жупи',
+	'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
