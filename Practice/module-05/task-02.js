62fa1b7133dcd9f982f66903a97062c7ac7d6e01
Sergey <domarch388640@gmail.com>
2021-12-17 15:53:32 +0200

    complete

--- Practice/module-05/task-02.js
+++ Practice/module-05/task-02.js
@@ -27,11 +27,11 @@ User ${имя} is ${возраст} years old and has ${кол-во подпис
 // console.log(User.prototype);
 
 
-// const mango = new User( 'Mango', 2, 20); // для проверки работоспособности кода
+// const mango = new User( 'Mango', 2, 20);
 // mango.getInfo(); // User Mango is 2 years old and has 20 followers
 
 
-// const poly = new User( 'Poly', 3, 17); // для проверки работоспособности кода
+// const poly = new User( 'Poly', 3, 17);
 // poly.getInfo(); // User Poly is 3 years old and has 17 followers
 
 
@@ -54,7 +54,7 @@ const mango = new User({
 	followers: 20,
 });
 
-mango.getInfo();
+mango.getInfo(); // User Mango is 2 years old and has 20 followers
 
 const poly = new User({
 	name: 'Poly',
@@ -62,4 +62,4 @@ const poly = new User({
 	followers: 17,
 });
 
-poly.getInfo();
+poly.getInfo(); // User Poly is 3 years old and has 17 followers
