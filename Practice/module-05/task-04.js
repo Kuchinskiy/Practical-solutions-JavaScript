2a167a8408d1716c19e62ed4fc99643f02a4f054
Sergey <domarch388640@gmail.com>
2021-12-26 11:39:52 +0200

    complete

--- Practice/module-05/task-04.js
+++ Practice/module-05/task-04.js
@@ -27,12 +27,18 @@ class StringBuilder {
 		this._value = str + this._value;
 	}
 
+	pad(str) {
+		this._value = `${str} ${this._value} ${str}`;
+	}
 }
 
 const builder = new StringBuilder('.');
+
 builder.append('^');
 console.log(builder.value); // '.^'
 
 builder.prepend('^');
 console.log(builder.value); // '^.^'
 
+builder.pad('=');
+console.log(builder.value); // =^.^=
