e472c2ff46cdadce3b0ec7de0718ea3961e92c07
Sergey <domarch388640@gmail.com>
2022-01-12 14:02:06 +0200

    complete

--- Practice/module-06/task-01.js
+++ Practice/module-06/task-01.js
@@ -1,5 +1,19 @@
 'use strict';
 
+import users from './users.js';
 /*
-
+Получить массив имен всех пользователей (поля(key) -->> name).
 */
+
+// map
+// const getUserNames = users => users.map(user => user.name);
+
+// reduce
+const getUserNames = users => users.reduce((acc,user) => {
+	acc.push(user.name);
+	return acc;
+}, []);
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head',
+// 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
