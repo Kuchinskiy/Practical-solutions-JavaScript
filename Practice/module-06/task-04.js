18cd849060c8b5a124302fbb4268c029076b3901
Sergey <domarch388640@gmail.com>
2022-01-13 16:36:04 +0200

    complete

--- Practice/module-06/task-04.js
+++ Practice/module-06/task-04.js
@@ -3,6 +3,17 @@
 import users from './users.js';
 
 /*
-Получить массив только неактивных пользователей (поле isActive).
+1.Получить массив объектов только неактивных пользователей (поле isActive).
+
+2.Получить массив имен, только неактивных пользователей (поле isActive).
 */
 
+const getInactiveUsers = users => users.filter((user) => user.isActive === false);
+// const getInactiveUsers = users => users.filter((user) => !user.isActive);
+
+// const getInactiveUsers = users => users.filter((user) => !user.isActive).map((user) => user.name);
+
+console.log(getInactiveUsers(users)); // [{…}, {…}, {…}]
+// [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+
+// ['Moore Hensley', 'Ross Vazquez', 'Blackburn Dotson']
