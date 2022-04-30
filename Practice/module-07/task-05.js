c957fd30bd3b162ea94f61bdd356afe46c5fa7ca
Sergey <domarch388640@gmail.com>
2022-04-30 19:29:12 +0300

    complete

--- Practice/module-07/task-05.js
+++ Practice/module-07/task-05.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('test');
+/*
+* Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input),
+* подставляет его текущее значение в span#name-output.
+*Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+
+<input type="text" placeholder="Ваше имя?" id="name-input" />
+<h1>Привет, <span id="name-output">незнакомец</span>!</h1>
+*/
+
