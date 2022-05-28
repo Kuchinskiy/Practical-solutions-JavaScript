f5ce6976b66068670556092cfa05d6fea2221ae6
Sergey <domarch388640@gmail.com>
2022-05-28 11:55:59 +0300

    complete

--- Practice/module-08/practice.js
+++ Practice/module-08/practice.js
@@ -1,3 +1,31 @@
 'use strict';
 
 // Delegation event.target
+const nav = document.querySelector('.js-nav');
+
+nav.addEventListener('click', handleNavClick);
+
+function handleNavClick(evt) {
+	evt.preventDefault();
+
+	const target = evt.target;
+	console.log('evt target', target); // смотрим что тут находится
+
+	// Проверяем тип узла, если не ссылка выходим из функции(для этого используем nodeName)
+	if (target.nodeName !== 'A') {
+		return;
+	}
+
+	setActiveLink(target);
+}
+
+
+function setActiveLink(nextActiveLink) {
+	const currentActiveLink = nav.querySelector('a.active');
+
+	if (currentActiveLink) {
+		currentActiveLink.classList.remove('active');
+	}
+
+	nextActiveLink.classList.add('active');
+}
