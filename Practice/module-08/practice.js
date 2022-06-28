abdc5d52e11eb69cad0a3f507ae69cfbca28a9d8
Sergey <domarch388640@gmail.com>
2022-06-28 16:33:18 +0300

    complete

--- Practice/module-08/practice.js
+++ Practice/module-08/practice.js
@@ -201,15 +201,23 @@ sections.forEach(section => {
 Подключаем библиотеку через Feature Detection, тем самым делаем 'lazy-load' кроссбраузерным
 */
 
-// Feature Detection
+// Feature Detection(вявление возможностей браузера) - Библиотека lazysizes
 if ('loading' in HTMLImageElement.prototype) {
+	addSrcAttrToLazyImages();
+} else {
+	addLazySizesScript();
+}
+
+function addSrcAttrToLazyImages() {
 	const images = document.querySelectorAll('img[loading="lazy"]');
 
 	images.forEach(img => {
 		// добавляем атрибут 'src' динамически, изначально его нет в разметке
 		img.src = img.dataset.src;
 	});
-} else {
+}
+
+function addLazySizesScript() {
 	const script = document.createElement('script');
 	script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
 	script.integrity = "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
@@ -219,6 +227,8 @@ if ('loading' in HTMLImageElement.prototype) {
 	document.body.appendChild(script);
 }
 
+
+// Intersection Observer
 
 const lazyLoad = target => {
 	const options = {
