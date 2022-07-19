9602eb53f04b037282b931691acaca21058b29e1
Sergey <domarch388640@gmail.com>
2022-07-19 23:07:51 +0300

    complete

--- Practice/module-08/gallery_project/gallery.js
+++ Practice/module-08/gallery_project/gallery.js
@@ -5,20 +5,21 @@ import images from './app.js';
 
 const refs = {
 	gallery: document.querySelector('.js-gallery'),
-	lightbox: document.querySelector('.js-lightbox'),
-	modalContent: document.querySelector('.lightbox__content'),
-	image: document.querySelector('.lightbox__image'),
-	btn: document.querySelector('[data-action="close-lightbox"]'),
-	img: document.createElement('img'),
+	// lightbox: document.querySelector('.js-lightbox'),
+	// modalContent: document.querySelector('.lightbox__content'),
+	// image: document.querySelector('.lightbox__image'),
+	// btn: document.querySelector('[data-action="close-lightbox"]'),
+	// img: document.createElement('img'),
 };
 
-// refs.gallery.addEventListener('click', handleClickUrlGallery);
+refs.gallery.addEventListener('click', handleClickUrlGallery);
 // refs.btn.addEventListener('click', handleClickOffModalBtn);
 // refs.modalContent.addEventListener('click', handleClickCloseModalContent);
 
-// function handleClickUrlGallery(evt) {
-
-// }
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+function handleClickUrlGallery(evt) {
+	evt.preventDefault();
+}
 
 // function handleClickOffModalBtn(evt) {
 
@@ -28,18 +29,21 @@ const refs = {
 
 // }
 
-// Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
+/*
+Варинт - 1
+Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
+*/
 const createImgElemMarkup = ({ preview, original, description }) => {
 	return `<li class="gallery__item">
 	<a
 	class="gallery__link"
-	href= ${original}
+	href="${original}"
 	>
 	<img
 		class="gallery__image"
-		src= ${preview}
-		data-source= ${original}
-		alt= ${description}
+		src="${preview}"
+		data-source="${original}"
+		alt="${description}"
 	/>
 	</a>
 </li>`;
@@ -47,4 +51,33 @@ const createImgElemMarkup = ({ preview, original, description }) => {
 
 const galleryMarkup = images.map(createImgElemMarkup).join('');
 refs.gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
 // refs.lightbox.classList.add('is-open');
+
+
+/*
+Вариант - 2
+Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
+*/  
+// const createImgElemMarkup = (images) => {
+// 	return images
+// 		.map(({ preview, original, description }) => {
+// 			return `<li class="gallery__item">
+// 	<a
+// 	class="gallery__link"
+// 	href="${original}"
+// 	>
+// 	<img
+// 		class="gallery__image"
+// 		src="${preview}"
+// 		data-source= ${original}
+// 		alt="${description}"
+// 	/>
+// 	</a>
+// </li>`;
+// 		})
+// 		.join('');
+// };
+
+// const markup = createImgElemMarkup(images);
+// refs.gallery.insertAdjacentHTML('afterbegin', markup);
